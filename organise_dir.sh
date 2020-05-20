d37a612e12f28c5704623da3608bf726de18e3e1
simonsoubeyrand <simonsoubeyrand@gmail.com>
2020-05-20 22:52:52 +0100

    cleared up useless text in bash file

--- organise_dir.sh
+++ organise_dir.sh
@@ -3,7 +3,7 @@
 #### Defining paths
 topdir=~/CharestLab/brainhackschool/deep_representational_learning
 data_rdm=~/CharestLab/brainhackschool/deep_representational_learning/data_rdms_matlab
-# dcm2niidir=/Users/franklinfeingold/Desktop/dcm2niix_3-Jan-2018_mac
+
 
 
 # Create dir rdms
@@ -15,17 +15,7 @@ rdms_dir=${topdir}/rdms
 jo -p "Name"="SuperRecogniser-U.Birmingham CharestLab Sample Dataset" "BIDSVersion"="1.0.2" >> ${topdir}/dataset_description.json
 
 
-####Anatomical Organization####
-#for subj in 3; do
-#	echo "Processing subject $subj"
+#### Organization ####
 
-###Create structure
-#mkdir -p ${niidir}/sub-${subj}/ses-1/
-
-###Convert dcm to nii
-#Only convert the Dicom folder anat
-#for direcs in anat; do
-#${dcm2niidir}/dcm2niix -o ${niidir}/sub-${subj} -f ${subj}_%f_%p ${dcmdir}/${subj}/${direcs}
-#done
 
 
