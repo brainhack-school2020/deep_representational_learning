0fc6c896863307d75853e0a3eb0852aaa65b6ffa
simonsoubeyrand <simonsoubeyrand@gmail.com>
2020-05-20 19:03:21 +0100

    started useful functions

---
+++ analysis/check_rdms.py
@@ -0,0 +1,6 @@
+from helpful_functions import average_subjects_rdms
+
+# load all rdms
+rdms_all = average_subjects_rdms(subject_array=[1,2,3],group=1,session=2)
+
+# average rdms
--- analysis/helpful_functions.py
+++ analysis/helpful_functions.py
@@ -1,3 +1,26 @@
-helpful_functions
+import scipy.io as sio
+from os import listdir
+from os.path import isfile, join
+import numpy as np
 
-def import_subjects_rdms
+
+def average_subjects_rdms(subject_array,group,session):
+    general_dir= '/home/adf/faghelss/CharestLab/brainhackschool/deep_representational_learning/'
+    rdm_dir=join(general_dir,'derivatives/data_rdms_matlab/')
+    
+    # group_dir=join(general_dir,f'/{group}/')
+    if group: 
+        grp_str='sub' 
+    else: 
+        grp_str='ctrl-sub'
+    
+    rdm_all=[]
+    for sub in subject_array:
+        
+        file_name=join(rdm_dir,f'rsa_decoding_{grp_str}{sub}_sess{session}.mat')
+        rdm=np.array(sio.loadmat(file_name)['rdm_time'])
+
+        rdm_all=np.concatenate((rdm_all,rdm),axis=0)
+
+        return  rdm_all
+
